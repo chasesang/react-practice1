a14436bcc06ea614d2f96e26a232d35faa12ddf5
Chase Sang <chase.sang@gmail.com>
2017-06-14 23:45:20 -0700

    more coponentsss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import Title from './Header/Title';
 
 class Header extends Component {
 
   render() {
 
     return (
-    <header>header</header>
+      <div>
+    <Title />
+  </div>
     );
   }
 }
